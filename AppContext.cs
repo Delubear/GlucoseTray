53ecbc68d1b37947ada54c1d60bd5458ddfc5d26
Chris Baker <Chris.baker@arrt.org>
2019-04-22 20:36:25 -0500

    Add project files.

---
+++ AppContext.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GlucoseTray
+{
+    public class AppContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        private readonly int HighBg = int.Parse(ConfigurationManager.AppSettings["HighBg"]);
+        private readonly int LowBg = int.Parse(ConfigurationManager.AppSettings["LowBg"]);
+        private GlucoseFetchResult FetchResult;
+
+        public AppContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Exit", Exit)
+            }),
+                Visible = true
+            };
+
+            // Pull new data every 5 minutes
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(5);
+            var timer = new System.Threading.Timer((_) => CreateIcon(), null, startTimeSpan, periodTimeSpan);
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void CreateTextIcon(string str)
+        {
+            Font fontToUse = new Font("Trebuchet MS", 10, FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = SetColor();
+
+            Bitmap bitmapText = new Bitmap(16, 16);
+            Graphics g = Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+
+            g.Clear(Color.Transparent);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(str, fontToUse, brushToUse, -2, 0);
+            hIcon = bitmapText.GetHicon();
+            trayIcon.Icon = Icon.FromHandle(hIcon);
+            //DestroyIcon(hIcon.ToInt32);
+        }
+
+        private Brush SetColor()
+        {
+            if (HighBg <= FetchResult.Value)
+                return new SolidBrush(Color.Yellow);
+            else if (LowBg >= FetchResult.Value)
+                return new SolidBrush(Color.Red);
+            else
+                return new SolidBrush(Color.White);
+        }
+
+        private void CreateIcon()
+        {
+            var service = new GlucoseFetch();
+            FetchResult = service.GetLatestReading();
+            trayIcon.Text = $"{FetchResult.Time.ToLongTimeString()}  {GetTrendArrow(FetchResult.Trend)}";
+            trayIcon.Click += ShowBalloon;
+            CreateTextIcon(FetchResult.Value.ToString());
+        }
+
+        private void ShowBalloon(object sender, EventArgs e)
+        {
+            trayIcon.ShowBalloonTip(2000, "Glucose", $"{FetchResult.Time.ToLongTimeString()}    {GetTrendArrow(FetchResult.Trend)}", ToolTipIcon.Info);
+        }
+
+        private string GetTrendArrow(string trend)
+        {
+            if (string.Equals(trend, "Flat", StringComparison.OrdinalIgnoreCase))
+                return "→";
+            else if (string.Equals(trend, "FortyFiveDown", StringComparison.OrdinalIgnoreCase))
+                return "↘";
+            else if (string.Equals(trend, "FortyFiveUp", StringComparison.OrdinalIgnoreCase))
+                return "↗";
+            else if (string.Equals(trend, "SingleDown", StringComparison.OrdinalIgnoreCase))
+                return "↓";
+            else if (string.Equals(trend, "SingleUp", StringComparison.OrdinalIgnoreCase))
+                return "↑";
+            else if (string.Equals(trend, "DoubleDown", StringComparison.OrdinalIgnoreCase))
+                return "⮇";
+            else if (string.Equals(trend, "DoubleUp", StringComparison.OrdinalIgnoreCase))
+                return "⮅";
+            else
+                return "";
+        }
+    }
+}
---
+++ GlucoseFetch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace GlucoseTray
+{
+    public class GlucoseFetch
+    {
+        private readonly string _request = ConfigurationManager.AppSettings["NightscoutUrl"] + "/api/v1/entries/sgv?count=1";
+
+        public GlucoseFetchResult GetLatestReading()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(_request).Result;
+            var content = response.Content.ReadAsStringAsync().Result.Split('\t');
+
+            Regex rgx = new Regex("[^a-zA-Z]");
+            var time = ParseDate(content[0]);
+
+            var trendString = rgx.Replace(content[3], "");
+            var val = int.Parse(content[2]);
+
+            return new GlucoseFetchResult()
+            {
+                Value = val,
+                Trend = trendString,
+                Time = time
+            };
+        }
+
+        private DateTime ParseDate(string date)
+        {
+            Regex rgx = new Regex("[^0-9]");
+
+            var ymd = date.Split('T')[0].Split('-');
+            var hms = date.Split('T')[1].Split('.')[0].Split(':');
+
+            var year = int.Parse(rgx.Replace(ymd[0], ""));
+            var month = int.Parse(rgx.Replace(ymd[1], ""));
+            var day = int.Parse(rgx.Replace(ymd[2], ""));
+
+            var hour = int.Parse(rgx.Replace(hms[0], ""));
+            var minute = int.Parse(rgx.Replace(hms[1], ""));
+            var second = int.Parse(rgx.Replace(hms[2], ""));
+
+            var dateTime = new DateTime(year, month, day, hour, minute, second, DateTimeKind.Utc);
+
+            return dateTime.ToLocalTime();
+        }
+    }
+
+    public class GlucoseFetchResult
+    {
+        public int Value { get; set; }
+        public string Trend { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
