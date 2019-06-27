54410111deafbe99c8d755028fb291a1e31c901d
Chris Baker <Chris.baker@arrt.org>
2019-06-27 08:32:28 -0500

    comments

--- Dexcom.Fetch/Models/GlucoseFetchResult.cs
+++ Dexcom.Fetch/Models/GlucoseFetchResult.cs
@@ -4,8 +4,19 @@ namespace Dexcom.Fetch.Models
 {
     public class GlucoseFetchResult
     {
+        /// <summary>
+        /// Glucose value
+        /// </summary>
         public int Value { get; set; }
+
+        /// <summary>
+        /// Arrow indicating direction
+        /// </summary>
         public string TrendIcon { get; set; }
+
+        /// <summary>
+        /// Time of result
+        /// </summary>
         public DateTime Time { get; set; }
     }
 }
