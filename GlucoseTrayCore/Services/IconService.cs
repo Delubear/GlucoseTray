2a0f3916151f8b59fd06335480c2c1e527b79ac6
Chris Baker <Chris.baker@arrt.org>
2020-04-28 12:26:44 -0500

    Enhancements to allow dynamic font-size and text offset based on our reading and configuration.

--- GlucoseTrayCore/Services/IconService.cs
+++ GlucoseTrayCore/Services/IconService.cs
@@ -1,10 +1,9 @@
-﻿using Dexcom.Fetch.Extensions;
+﻿using Dexcom.Fetch.Enums;
+using Dexcom.Fetch.Extensions;
 using Dexcom.Fetch.Models;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Drawing.Text;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -12,8 +11,12 @@ namespace GlucoseTrayCore.Services
 {
     public class IconService
     {
-        private readonly Font fontToUse = new Font("Tahoma", 34, FontStyle.Regular, GraphicsUnit.Pixel);
         private readonly ILogger _logger;
+        private readonly float _standardOffset = -3f;
+        private readonly int _defaultFontSize = 10;
+        private readonly int _smallerFontSize = 9;
+        private Font _fontToUse;
+        private bool _useDefaultFontSize = true;
 
         public IconService(ILogger logger) => _logger = logger;
 
@@ -48,11 +51,14 @@ namespace GlucoseTrayCore.Services
                 result = "DAN";
             }
 
-            var bitmapText = new Bitmap(64, 64);
+            var xOffset = CalculateXPosition(fetchResult);
+            var fontSize = _useDefaultFontSize ? _defaultFontSize : _smallerFontSize;
+            _fontToUse = new Font("Roboto", fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            var bitmapText = new Bitmap(16, 16);
             var g = Graphics.FromImage(bitmapText);
             g.Clear(Color.Transparent);
-            g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-            g.DrawString(result, fontToUse, SetColor(fetchResult.Value), -6f, 5f);
+            g.DrawString(result, _fontToUse, SetColor(fetchResult.Value), xOffset, 0f);
             var hIcon = bitmapText.GetHicon();
             var myIcon = Icon.FromHandle(hIcon);
             trayIcon.Icon = myIcon;
@@ -61,6 +67,22 @@ namespace GlucoseTrayCore.Services
             bitmapText.Dispose();
             g.Dispose();
             myIcon.Dispose();
+        }
+
+        private float CalculateXPosition(GlucoseFetchResult result)
+        {
+            _useDefaultFontSize = true;
+            var value = result.Value;
+            if (result.UnitDisplayType == GlucoseUnitType.MG) // Non MMOL display, use our standard offset.
+                return _standardOffset;
+            if (value > 9.9 && value < 20) // MMOL display with 3 digits, use a greater offset to fit display.
+                return -5f;
+            if (value > 19.9) // MMOL with 3 digits over 20.  Since 2 is a lot wider than 1, this requires also changing the font size from 10 to 9.
+            {
+                _useDefaultFontSize = false;
+                return _standardOffset;
+            }
+            return _standardOffset; // MMOL display with only two digits, use our standard offset.
         }
     }
 }
