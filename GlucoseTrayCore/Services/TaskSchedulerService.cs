c9bd1ce6d9e680077bb47f8806cc0c201729de95
Chris Baker <Chris.baker@arrt.org>
2020-10-14 23:04:52 -0500

    Catches for if the .exe gets moved and we still want our run at startup to work (after a manual run)

--- GlucoseTrayCore/Services/TaskSchedulerService.cs
+++ GlucoseTrayCore/Services/TaskSchedulerService.cs
@@ -7,14 +7,26 @@ namespace GlucoseTrayCore.Services
 {
     public class TaskSchedulerService
     {
-        private static readonly string ExecutablePath = Process.GetCurrentProcess().MainModule.FileName; // Environment.ProcessPath should be available in the future for this
-        private static readonly string TaskName = "GlucoseTray-" + Environment.UserName;
+        private readonly string ExecutablePath = "\"" + Process.GetCurrentProcess().MainModule.FileName + "\""; // Environment.ProcessPath should be available in the future for this
+        private readonly string TaskName = "GlucoseTray-" + Environment.UserName;
+        private readonly string WorkingDirectory = Directory.GetCurrentDirectory();
 
         public bool HasTaskEnabled()
         {
             using var ts = new TaskService();
             var existingTask = ts.GetTask(TaskName);
-            return existingTask?.Enabled == true;
+            if (existingTask is null)
+                return false;
+
+            var action = (ExecAction)existingTask.Definition.Actions[0];
+            if (action.Path != ExecutablePath || action.WorkingDirectory != Directory.GetCurrentDirectory()) // File has been moved since task was created. Update values.
+            {
+                action.Path = ExecutablePath;
+                action.WorkingDirectory = WorkingDirectory;
+                existingTask.RegisterChanges();
+            }
+
+            return existingTask.Enabled;
         }
 
         public void ToggleTask(bool enable)
@@ -24,7 +36,7 @@ namespace GlucoseTrayCore.Services
             if (task is null)
             {
                 var trigger = new LogonTrigger(){ UserId = Environment.UserName };
-                var action = new ExecAction("\"" + ExecutablePath + "\"", workingDirectory: Directory.GetCurrentDirectory());
+                var action = new ExecAction(ExecutablePath, workingDirectory: WorkingDirectory);
                 var description = "GlucoseTray task for " + Environment.UserName;
                 task = ts.AddTask(TaskName, trigger, action, description: description);
                 task.Definition.Settings.ExecutionTimeLimit = TimeSpan.FromSeconds(0); // Don't end task after 72 hour default runtime.
