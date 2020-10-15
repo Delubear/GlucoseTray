79705f203b16aa9fb6ca30f5af621a5a6d9ef099
Chris Baker <Chris.baker@arrt.org>
2020-10-14 22:44:10 -0500

    Prevent 72 max running time limit for task.

--- GlucoseTrayCore/Services/TaskSchedulerService.cs
+++ GlucoseTrayCore/Services/TaskSchedulerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32.TaskScheduler;
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace GlucoseTrayCore.Services
 {
@@ -19,7 +20,16 @@ namespace GlucoseTrayCore.Services
         public void ToggleTask(bool enable)
         {
             using var ts = new TaskService();
-            var task = ts.GetTask(TaskName) ?? ts.AddTask(TaskName, new LogonTrigger(){ UserId = Environment.UserName }, new ExecAction("\"" + ExecutablePath + "\""));
+            var task = ts.GetTask(TaskName);
+            if (task is null)
+            {
+                var trigger = new LogonTrigger(){ UserId = Environment.UserName };
+                var action = new ExecAction("\"" + ExecutablePath + "\"", workingDirectory: Directory.GetCurrentDirectory());
+                var description = "GlucoseTray task for " + Environment.UserName;
+                task = ts.AddTask(TaskName, trigger, action, description: description);
+                task.Definition.Settings.ExecutionTimeLimit = TimeSpan.FromSeconds(0); // Don't end task after 72 hour default runtime.
+                task.RegisterChanges();
+            }
             task.Enabled = enable;
         }
     }
